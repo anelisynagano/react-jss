e9735bc8bc3933966cd0a34ac2fa7479c2a3f72a
WildCodeSchool <anelisynagano@gmail.com>
2020-10-16 21:39:40 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import here injectSheet from react-jss library
+
+// create a variable styles and create a stylesheet for the following classes and apply the properties:
+// - list: display: flex; list-style-type: none; (tip: don't forget to camelCase the property!)
+// - title: color: #39424e;
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // add the correct className values to your ul and h1 tags
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <h1>React JSS!</h1>
+      <p>This is an example of React JSS</p>
     </div>
   );
 }
 
+// don't forget to wrap your component with the imported injectSheet!
 export default App;
